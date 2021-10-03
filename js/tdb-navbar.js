2dae547de31dc07697a1eed4fc92ccd1a8d48a3b
mark-terminusdb <86666742+mark-terminusdb@users.noreply.github.com>
2021-10-03 21:01:44 +0100

    Update tdb-navbar.js

--- js/tdb-navbar.js
+++ js/tdb-navbar.js
@@ -3,7 +3,7 @@ function tdb_nav()
     var sMenu = '\
 <div class="tdb-nav">\
     <span class="tdb-nav-span"><a class="tdb-k tdb-k-h2" href="https://terminusdb.com/">Home</a></span>\
-    <span class="tdb-nav-span"><a class="tdb-k tdb-k-h2" href="/">Docs Home</a></span>\
+    <span class="tdb-nav-span"><a class="tdb-k tdb-k-h2" href="https://docs.terminusdb.com/next/#/">Docs Home</a></span>\
     <span class="tdb-nav-span"><a class="tdb-k tdb-k-h2" href="#">Roadmap</a></span>\
     <span class="tdb-nav-span"><a class="tdb-k tdb-k-h2" href="#">Glossary</a></span>\
     <span class="tdb-nav-span"><a class="tdb-k tdb-k-h2" href="#">FAQ</a></span>\
