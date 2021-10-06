50688c5bab483cdd54e44d12b791808a0b659db3
mark-terminusdb <86666742+mark-terminusdb@users.noreply.github.com>
2021-10-06 16:24:12 +0100

    Update tdb-navbar.js

--- js/tdb-navbar.js
+++ js/tdb-navbar.js
@@ -4,10 +4,10 @@ function tdb_nav()
 <div class="tdb-nav">\
     <span class="tdb-nav-span"><a class="tdb-k tdb-k-h2" href="https://terminusdb.com/">Home</a></span>\
     <span class="tdb-nav-span"><a class="tdb-k tdb-k-h2" href="#">Docs Home</a></span>\
-    <span class="tdb-nav-span"><a class="tdb-k tdb-k-h2" href="#">Roadmap</a></span>\
-    <span class="tdb-nav-span"><a class="tdb-k tdb-k-h2" href="#">Glossary</a></span>\
-    <span class="tdb-nav-span"><a class="tdb-k tdb-k-h2" href="#">FAQ</a></span>\
-    <span class="tdb-nav-span"><a class="tdb-k tdb-k-h2" href="#">Contact Us</a></span>\
+    <span class="tdb-nav-span" style="color: gray" title="Coming soon...">Roadmap</span>\
+    <span class="tdb-nav-span" style="color: gray" title="Coming soon...">Glossary</span>\
+    <span class="tdb-nav-span" style="color: gray" title="Coming soon...">FAQ</span>\
+    <span class="tdb-nav-span"><a class="tdb-k tdb-k-h2" href="https://terminusdb.com/contact/">Contact Us</span>\
 </div>\
 <br><br>\n\n';
 
