b0dddac97e6c5355e58d89ea3a83e9a16dc59735
mark-terminusdb <86666742+mark-terminusdb@users.noreply.github.com>
2021-11-29 10:01:28 +0000

    Add files via upload

--- js/tdb-cover.js
+++ js/tdb-cover.js
@@ -1,4 +1,6 @@
-function tdb_nav_cover(o)
+// window.addEventListener("resize", function() { tdb_cvrImage(); });
+
+function tdb_cvrMain(o)
 {
     document.getElementById("id-cvr").style.opacity = (o == 1 ? o : 0.8);
 
@@ -11,10 +13,9 @@ function tdb_nav_cover(o)
     document.getElementById("id-07").style.opacity = o;
     document.getElementById("id-08").style.opacity = o;
     document.getElementById("id-09").style.opacity = o;
-
 }
 
-function tdb_cvr_p(o)
+function tdb_cvrSub(o)
 {
     document.getElementById("id-cvr-pgs").style.opacity = (o == 1 ? o : 0.8);
 
@@ -29,4 +30,25 @@ function tdb_cvr_p(o)
 function tdb_hide()
 {
     document.getElementById("id-nav").style.visibility = "hidden";
+}
+
+function tdb_cvrImage()
+{
+    var div1 = document.getElementById("id-cvr").style,
+        div2 = document.getElementById("id-cvr-pgs").style;
+
+    if (window.innerWidth < 1034)
+    {
+        div1.visibility = "hidden";
+        div2.visibility = "hidden";
+        div1.height     = "0px";
+        div2.height     = "0px";
+    }
+    else
+    {
+        div1.visibility = "visible";
+        div2.visibility = "visible";
+        div1.height     = "810px"; 
+        div2.height     = "710px";       
+    }
 }
