0f03b55b36de162516d0960a750005c560556dbe
Sean Leather <sean@terminusdb.com>
2021-12-08 09:52:18 +0200

    CI: Build terminusdb with docker instead of make
    `make all` wasn't producing a `terminusdb` executable for some reason,
    so the reference-cli.md wasn't get filled with the command descriptions
    from `terminusdb help -m`. This change replaces `make` with `docker
    build` and `docker run`.

--- .github/generate_docs.sh
+++ .github/generate_docs.sh
@@ -5,13 +5,13 @@
 # 1. runs the script in `jsonToMDConverter` to build reference schema Markdown
 #    files from the JSON sources
 #
-# 2. runs `ronn` to build a Markdown file from the command-line man page
+# 2. generates a CLI reference Markdown file from the template
 
 set -e
 
 BRANCH="$1"
 
-[[ "$BRANCH" ]] || (echo "Missing argument for branch"; exit 1)
+[[ "$BRANCH" ]] || (echo -e "\033[0;34mMissing argument for branch\033[0m"; exit 1)
 
 # Use 'main' if the branch is not a version branch (e.g. 'v10.1') or 'main'.
 if [[ $(echo "$BRANCH" | grep -vE "^main|^v") ]]; then
@@ -22,37 +22,41 @@ TOP_DIR="$(git rev-parse --show-toplevel)"
 REFERENCE_DIR="$TOP_DIR/md/reference"
 TERMINUSDB_DIR="$TOP_DIR/terminusdb"
 
-echo "Checking out 'terminusdb/terminusdb' at branch '$BRANCH'..."
+echo -e "\033[0;34mChecking out 'terminusdb/terminusdb' at branch '$BRANCH'...\033[0m"
 
 # Check out the terminusdb repository
 git clone -b "$BRANCH" --depth 1 \
   https://github.com/terminusdb/terminusdb.git \
   "$TERMINUSDB_DIR"
 
-echo "Building reference schema documentation..."
+echo -e "\033[0;34mBuilding reference schema documentation...\033[0m"
 
 SCHEMA_SRC_DIR="$TERMINUSDB_DIR/src/terminus-schema"
-SCHEMA_DST_DIR="$REFERENCE_DIR"
 
 cd "$TOP_DIR/.github/jsonToMDConverter"
 npm install
-npm test -- "$SCHEMA_SRC_DIR/woql.json"          "$SCHEMA_DST_DIR/reference-woql-schema.md"
-npm test -- "$SCHEMA_SRC_DIR/ref.json"           "$SCHEMA_DST_DIR/reference-ref-schema.md"
-npm test -- "$SCHEMA_SRC_DIR/repository.json"    "$SCHEMA_DST_DIR/reference-repository-schema.md"
-npm test -- "$SCHEMA_SRC_DIR/system_schema.json" "$SCHEMA_DST_DIR/reference-system-schema.md"
+npm test -- "$SCHEMA_SRC_DIR/woql.json"          "$REFERENCE_DIR/reference-woql-schema.md"
+npm test -- "$SCHEMA_SRC_DIR/ref.json"           "$REFERENCE_DIR/reference-ref-schema.md"
+npm test -- "$SCHEMA_SRC_DIR/repository.json"    "$REFERENCE_DIR/reference-repository-schema.md"
+npm test -- "$SCHEMA_SRC_DIR/system_schema.json" "$REFERENCE_DIR/reference-system-schema.md"
 
-echo "Installing ronn (if needed)..."
+echo -e "\033[0;34mBuilding CLI documentation...\033[0m"
 
-command -v ronn > /dev/null 2>&1 || sudo apt install ronn
+cd "$TERMINUSDB_DIR"
+docker build . --file Dockerfile --tag terminusdb:local
+export HELP=$(docker run terminusdb:local /app/terminusdb/terminusdb help -m)
+envsubst < "./docs/terminusdb.1.ronn.template" > "$REFERENCE_DIR/reference-cli.md"
 
-echo "Building CLI documentation..."
-
-make -C "$TERMINUSDB_DIR" all
-cp "$TERMINUSDB_DIR/docs/terminusdb.1.ronn" "$REFERENCE_DIR/reference-cli.md"
-
-echo "Removing untracked files..."
+echo -e "\033[0;34mRemoving untracked files...\033[0m"
 
 cd "$TOP_DIR"
 git clean -d -f -f
 
-echo "Done."
+date
+echo -e "\033[0;34mI generated these files:\033[0m"
+ls -l \
+  "$REFERENCE_DIR/reference-cli.md" \
+  "$REFERENCE_DIR/reference-ref-schema.md" \
+  "$REFERENCE_DIR/reference-repository-schema.md" \
+  "$REFERENCE_DIR/reference-system-schema.md" \
+  "$REFERENCE_DIR/reference-woql-schema.md"
