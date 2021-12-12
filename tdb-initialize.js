05cc8b5297033d5446d3f803163febaa04bbe8c5
mark-terminusdb <86666742+mark-terminusdb@users.noreply.github.com>
2021-12-12 23:44:27 +0000

    Add files via upload

---
+++ tdb-initialize.js
@@ -0,0 +1,69 @@
+console.log("Initialize...");
+
+function tdb_checkLocalStore()
+{
+    console.log("Check local store 1...");
+    
+    if (tdb_localStore()) 
+    {
+        //  Include search plugin if local storage is enabled.
+
+        var script = document.createElement('script');
+
+        script.src = 'https://cdn.jsdelivr.net/npm/docsify/lib/plugins/search.min.js';
+        script.async = false;
+            
+        document.head.appendChild(script);
+    }
+}
+
+function tdb_localStore() 
+{
+    console.log("Check local store 2...");
+
+    try 
+    {
+      localStorage.setItem('x', 'x');
+      localStorage.removeItem('x');
+
+      return true;
+    } 
+    catch (e)
+    { 
+      return false;
+    }
+}
+
+function tdb_title() 
+{
+    const cSfx = " - TerminusDB Docs";
+
+    // Set title and hide navbar on home page.
+
+    if (location.hash == "#/")
+    {
+        document.title = `Home${cSfx}`;
+        
+        tdb_cvrHide("nav");
+        tdb_cvrHide("theme");
+    }
+    else
+        document.title += cSfx;
+}
+
+function tdb_adjust()
+{
+    var d = document.getElementById("main").style;
+    
+    d.marginLeft = "50px";
+    d.marginTop  = "65px";
+    d.float      = "left";
+}
+
+function tdb_theme(t) 
+{
+    var cssLnk = document.getElementsByTagName("link").item(3);
+    const cssList = ["dark", "light"];
+
+    cssLnk.href = `css/terminusdb-docs-${cssList[t]}.css`;
+}
