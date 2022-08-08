79ed9ee13e6905f4490663f9a100d4bd399b643d
Robin de Rooij <rrooij@users.noreply.github.com>
2022-08-08 15:09:41 +0200

    Add automatic generation from openapi spec (#156)

---
+++ .github/convert_gitbook.py
@@ -0,0 +1,48 @@
+import sys
+import yaml
+
+def main():
+    if len(sys.argv) < 3:
+        print("Please provide a filename and version as arguments")
+        sys.exit(1)
+    filename = sys.argv[1]
+    version = sys.argv[2]
+    process_yaml(filename, version)
+
+
+def process_yaml(yaml_file, version):
+    tags = {}
+    with open(yaml_file, 'r') as f:
+        data = yaml.safe_load(f)
+        for path, methods in data['paths'].items():
+            for method, method_object in methods.items():
+                if method not in ['get', 'post', 'delete', 'put', 'head']:
+                    continue
+                generated_entry = generate_gitbook(path, method, version)
+                tag = method_object['tags'][0]
+                if tag not in tags:
+                    tags[tag] = [generated_entry]
+                else:
+                    tags[tag].append(generated_entry)
+    print_all(tags)
+
+def generate_gitbook(path, method, version):
+    url = f'https://raw.githubusercontent.com/terminusdb/terminusdb/{version}/docs/openapi.yaml'
+    return f'''
+{{% swagger src="{url}" path="{path}" method="{method}" %}}
+[{url}]({url})
+{{% endswagger %}}
+
+'''
+
+
+def print_all(tags):
+    print("# HTTP API\n")
+    for tag, values in tags.items():
+        print(f"## {tag}")
+        for entry in values:
+            print(entry)
+
+
+if __name__ == '__main__':
+    main()
